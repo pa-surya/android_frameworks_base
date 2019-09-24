1d9931f92570b28d98c1d45b654cf8276a917f6c
Marko Man <darkobas@gmail.com>
2019-09-24 14:46:01 +0200

    base: Double tap to sleep on statusbar [1/2]
    arrow-11.0 edits:
    - Adapt code to refactored statusbar controllers.
    
    wave edits:
    - Adapt to existing dt2s on lockscreen
    
    commit f3321f6a498b3b55a509524183ce83b7503cf936
    Author: maxwen <max.weninger@gmail.com>
    Date:   Wed May 23 21:55:04 2018 +0200
    
        base: SystemUI: prevent that double tap to sleep triggers panel expand
    
        arrow-11.0 edits:
            - Adapt code to refactored statusbar controllers.
    
        http://jira.omnirom.org/browse/OMNI-1466
    
        Change-Id: If304577d666ebf8423cca1298dfbe044843657cc
    
    Change-Id: Ib2f884047dd3754e96152148ea9bd042374a2045
    
    [ghostrider-reborn | imjyotiraditya] adapted to android 12
    
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>
    Change-Id: Ibfcd4619654910a95e6d49acbbceb15e21e2d2d0

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -668,6 +668,8 @@ public class NotificationPanelViewController extends PanelViewController {
         }
     };
 
+    private int mStatusBarHeaderHeight;
+
     @Inject
     public NotificationPanelViewController(NotificationPanelView view,
             @Main Resources resources,
@@ -974,6 +976,8 @@ public class NotificationPanelViewController extends PanelViewController {
         mLockscreenNotificationQSPadding = mResources.getDimensionPixelSize(
                 R.dimen.notification_side_paddings);
         mUdfpsMaxYBurnInOffset = mResources.getDimensionPixelSize(R.dimen.udfps_burn_in_offset_y);
+        mStatusBarHeaderHeight = mResources.getDimensionPixelSize(
+                R.dimen.status_bar_height);
     }
 
     private void updateViewControllers(KeyguardStatusView keyguardStatusView,
@@ -3930,6 +3934,15 @@ public class NotificationPanelViewController extends PanelViewController {
 
                 if (mBarState == StatusBarState.KEYGUARD && !mPulsing && !mDozing) {
                     mDoubleTapGestureListener.onTouchEvent(event);
+                } else if (!mQsExpanded
+                        && mDoubleTapToSleepEnabled
+                        && event.getY() < mStatusBarHeaderHeight) {
+                    mDoubleTapGestureListener.onTouchEvent(event);
+                    // quick pulldown can trigger those values
+                    // on double tap - so reset them
+                    mQsExpandImmediate = false;
+                    requestPanelHeightUpdate();
+                    setListening(false);
                 }
 
                 // Make sure the next touch won't the blocked after the current ends.
@@ -4807,5 +4820,9 @@ public class NotificationPanelViewController extends PanelViewController {
     /** Returns the handler that the status bar should forward touches to. */
     public PhoneStatusBarView.TouchEventHandler getStatusBarTouchEventHandler() {
         return mStatusBarViewTouchEventHandler;
+    }
+
+    public void updateDoubleTapToSleep(boolean doubleTapToSleepEnabled) {
+        mDoubleTapToSleepEnabled = doubleTapToSleepEnabled;
     }
 }
