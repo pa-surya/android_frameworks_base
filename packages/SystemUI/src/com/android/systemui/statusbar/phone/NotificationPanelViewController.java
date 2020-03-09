58f9faab6ed52fd14bf86fed6fc7ab2a32c4bd11
Pranav Vashi <neobuddy89@gmail.com>
2020-03-10 01:12:12 +0530

    SystemUI: Keyguard: Remove unnecessary battery meter impl
    * Battery Meter View is itself capable is to decide when to refresh. No need to re-implement same in keyguard status bar.
    * This fixes spam on charging when displaying keyguard:
    
    3-10 00:23:34.928  2626  2626 W View    : requestLayout() improperly called by android.widget.TextView{63bad7b V.ED..... ......ID 38,0-128,105 #7f0a00a7 app:id/battery_percentage_view} during layout: running second layout pass
    03-10 00:23:34.933  2626  2626 W View    : requestLayout() improperly called by android.widget.TextView{63bad7b V.ED..... ........ 38,0-128,105 #7f0a00a7 app:id/battery_percentage_view} during second layout pass: posting in next frame
    03-10 00:23:34.944  2626  2626 W View    : requestLayout() improperly called by android.widget.TextView{63bad7b V.ED..... ......ID 38,0-128,105 #7f0a00a7 app:id/battery_percentage_view} during layout: running second layout pass
    03-10 00:23:34.953  2626  2626 W View    : requestLayout() improperly called by android.widget.TextView{63bad7b V.ED..... ........ 38,0-128,105 #7f0a00a7 app:id/battery_percentage_view} during second layout pass: posting in next frame
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Change-Id: I5a12eaae2ae3046a8a99b3c7e3041fcd257cab78

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelViewController.java
@@ -2358,7 +2358,6 @@ public class NotificationPanelViewController extends PanelViewController {
     }
 
     private void setListening(boolean listening) {
-        mKeyguardStatusBar.setListening(listening);
         if (mQs == null) return;
         mQs.setListening(listening);
     }
