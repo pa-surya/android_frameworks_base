c9f7cc099e7a0d21fbf5f36a1a83dc69c61a1a6f
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-01-28 22:12:40 +0530

    PixelPropsUtils: Add netflix to pixel spoofing list
     * unlocks hdr support
    
    Change-Id: If38861f351308658509e009e1841ef556b4f7542

--- core/java/com/android/internal/util/PixelPropsUtils.java
+++ core/java/com/android/internal/util/PixelPropsUtils.java
@@ -40,6 +40,7 @@ public class PixelPropsUtils {
     private static final Map<String, ArrayList<String>> propsToKeep;
     private static final String[] extraPackagesToChange = {
         "com.android.chrome"
+        "com.netflix.mediaclient"
     };
     private static final String[] packagesToChangeP6 = {
         "com.google.android.gms"
