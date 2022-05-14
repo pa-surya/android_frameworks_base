AOSPA/android_frameworks_base-old

commit 1b6e12f01dd6a17bf6d7e3aef08e95e38116f641

SystemUI: Show notification row when it's unliked to ringer volume
Signed-off-by: Mesquita <mesquita@aospa.co>
Change-Id: Id7a2602153dbc94eb40caf70946d2b31e4a3c19f

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1622,6 +1622,7 @@ public class VolumeDialogImpl implements VolumeDialog,
     private boolean isExpandableRowH(VolumeRow row) {
         return row != null && row != mDefaultRow && !row.defaultStream
                 && (row.stream == STREAM_RING
+                        || row.stream == STREAM_NOTIFICATION
                         || row.stream == STREAM_ALARM
                         || row.stream == STREAM_MUSIC);
     }

pa-surya/android_frameworks_base

commit 6cd8479d099f543419955abf03476ea84a1dc73d

SystemUI: Show notification row only when unlinked
Commit 1b6e12f01dd6a17bf6d7e3aef08e95e38116f641
("SystemUI: Show notification row when it's unliked to ringer volume")
caused notification row to show in expanded volume panel
even when notification and ring volumes are linked. Fix
it so that it shows only when they are unlinked.

Change-Id: I1d4768974c07247bae38470260f7917da44bd911

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -1622,7 +1622,7 @@ public class VolumeDialogImpl implements VolumeDialog,
     private boolean isExpandableRowH(VolumeRow row) {
         return row != null && row != mDefaultRow && !row.defaultStream
                 && (row.stream == STREAM_RING
-                        || row.stream == STREAM_NOTIFICATION
+                        || (row.stream == STREAM_NOTIFICATION && !mState.linkedNotification)
                         || row.stream == STREAM_ALARM
                         || row.stream == STREAM_MUSIC);
     }