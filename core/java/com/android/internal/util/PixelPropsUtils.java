ef0d0153acd6f712f4b31e32779d2c5b41545373
jhenrique09 <jhsv09@gmail.com>
2022-06-08 01:44:36 +0000

    PixelPropsUtils: Disable spoof for google recorder
    Change-Id: If0e8cc40483455666aac0abb57da99d53df57f8c
    (cherry picked from commit ec941056af6633cddbe7564d43a118b0d631f61e)
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- core/java/com/android/internal/util/PixelPropsUtils.java
+++ core/java/com/android/internal/util/PixelPropsUtils.java
@@ -63,7 +63,8 @@ public class PixelPropsUtils {
         "com.google.android.MTCL83",
         "com.google.android.UltraCVM",
         "com.google.android.apps.cameralite",
-        "com.google.android.apps.wearables.maestro.companion"
+        "com.google.android.apps.wearables.maestro.companion",
+        "com.google.android.apps.recorder"
     };
 
     static {
