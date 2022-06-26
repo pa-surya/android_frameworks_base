936993e8965cc23c8f451a96a2e979522d5a4684
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-06-26 17:32:11 +0530

    [DNM] SystemUI: Do not hide mobile signal in expanded QS
    After hiding it and adding it back when collapsed, its not
    placed correctly which looks ugly. Since we have a lot of
    space anyway, lets just keep the signal icons even when
    QS is expanded (until someone finds a real fix).
    
    Change-Id: I6cf4302896b1a858fd3ccf2ca36904ebdcf68f00

--- packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
+++ packages/SystemUI/src/com/android/systemui/qs/QuickStatusBarHeader.java
@@ -406,9 +406,6 @@ public class QuickStatusBarHeader extends FrameLayout implements
                     @Override
                     public void onAnimationAtEnd() {
                         super.onAnimationAtEnd();
-                        if (!mIsSingleCarrier) {
-                            mIconContainer.addIgnoredSlots(mRssiIgnoredSlots);
-                        }
                         // Make it gone so there's enough room for carrier names
                         mClockDateView.setVisibility(View.GONE);
                     }
@@ -418,9 +415,6 @@ public class QuickStatusBarHeader extends FrameLayout implements
                         mClockDateView.setVisibility(View.VISIBLE);
                         mClockDateView.setFreezeSwitching(true);
                         setSeparatorVisibility(false);
-                        if (!mIsSingleCarrier) {
-                            mIconContainer.addIgnoredSlots(mRssiIgnoredSlots);
-                        }
                     }
 
                     @Override
@@ -429,8 +423,6 @@ public class QuickStatusBarHeader extends FrameLayout implements
                         mClockDateView.setFreezeSwitching(false);
                         mClockDateView.setVisibility(View.VISIBLE);
                         setSeparatorVisibility(mShowClockIconsSeparator);
-                        // In QQS we never ignore RSSI.
-                        mIconContainer.removeIgnoredSlots(mRssiIgnoredSlots);
                     }
                 });
         mAlphaAnimator = builder.build();
