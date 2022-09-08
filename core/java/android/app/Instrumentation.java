98d9a1c419f72515dcc23d48b8fbc4815af1f165
Adithya R <gh0strider.2k18.reborn@gmail.com>
2022-09-08 16:02:28 +0530

    base: Purge PixelPropsUtils
    Goodbye, old friend.

--- core/java/android/app/Instrumentation.java
+++ core/java/android/app/Instrumentation.java
@@ -56,7 +56,6 @@ import android.view.Window;
 import android.view.WindowManagerGlobal;
 
 import com.android.internal.content.ReferrerIntent;
-import com.android.internal.util.PixelPropsUtils;
 
 import java.io.File;
 import java.lang.annotation.Retention;
@@ -1190,7 +1189,6 @@ public class Instrumentation {
         Application app = getFactory(context.getPackageName())
                 .instantiateApplication(cl, className);
         app.attach(context);
-        PixelPropsUtils.setProps(app.getPackageName());
         return app;
     }
     
@@ -1208,7 +1206,6 @@ public class Instrumentation {
             ClassNotFoundException {
         Application app = (Application)clazz.newInstance();
         app.attach(context);
-        PixelPropsUtils.setProps(app.getPackageName());
         return app;
     }
 
